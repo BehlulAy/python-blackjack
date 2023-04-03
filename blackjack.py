b8da4c5dca140d27ee66c12bbfcad724c1adc4e6
BehlulAy <behlulay@protonmail.com>
2023-04-03 11:33:06 +0300

    Create blackjack.py

---
+++ blackjack.py
@@ -0,0 +1,73 @@
+import random
+
+# Function to calculate the total value of a hand
+def hand_value(hand):
+    total = 0
+    aces = 0
+    for card in hand:
+        if card in ['J', 'Q', 'K']:
+            total += 10
+        elif card == 'A':
+            aces += 1
+        else:
+            total += int(card)
+    total += aces
+    while total > 21 and aces > 0:
+        total -= 10
+        aces -= 1
+    return total
+
+# Function to play the game
+def play_game():
+    deck = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'] * 4
+    random.shuffle(deck)
+    player_hand = []
+    dealer_hand = []
+    player_hand.append(deck.pop())
+    dealer_hand.append(deck.pop())
+    player_hand.append(deck.pop())
+    dealer_hand.append(deck.pop())
+    print(f"Player's hand: {player_hand}")
+    print(f"Dealer's hand: ['?', '{dealer_hand[1]}']")
+    while True:
+        choice = input("Do you want to hit or stand? ")
+        if choice.lower() == 'hit':
+            player_hand.append(deck.pop())
+            print(f"Player's hand: {player_hand}")
+            if hand_value(player_hand) > 21:
+                print("Bust! Dealer wins.")
+                return False
+        elif choice.lower() == 'stand':
+            break
+    while hand_value(dealer_hand) < 17:
+        dealer_hand.append(deck.pop())
+    print(f"Player's hand: {player_hand}")
+    print(f"Dealer's hand: {dealer_hand}")
+    if hand_value(dealer_hand) > 21:
+        print("Dealer busts! Player wins.")
+        return True
+    elif hand_value(dealer_hand) > hand_value(player_hand):
+        print("Dealer wins.")
+        return False
+    elif hand_value(player_hand) > hand_value(dealer_hand):
+        print("Player wins.")
+        return True
+    else:
+        print("Push (tie).")
+        return None
+
+# Main loop
+money = 100
+while money > 0:
+    print(f"Your current balance is {money}.")
+    bet = int(input("How much do you want to bet? "))
+    if bet > money:
+        print("You don't have enough money for that bet.")
+        continue
+    result = play_game()
+    if result == True:
+        money += bet
+    elif result == False:
+        money -= bet
+    else:
+        continue
